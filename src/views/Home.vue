40fae011dea6d09d347e349a0627417bb9f62873
aditcahyono19 <aditcahyono12@gmail.com>
2023-12-14 23:03:47 +0700

    rev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,7 +51,7 @@ export default {
   },
   mounted() {
     axios
-    .get('http://https://my-json-server.typicode.com/aditcahyono19/katalogmenu', this.data, {
+    this.axios.get('http://https://my-json-server.typicode.com/aditcahyono19/katalogmenu', this.data, {
    headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
