8870fe4ba71ff3a9683a4c162b010cea09bb4eb7
aditcahyono19 <aditcahyono12@gmail.com>
2023-12-30 18:45:12 +0700

    1

--- src/views/MenuDetail.vue
+++ src/views/MenuDetail.vue
@@ -38,13 +38,23 @@
           <form class="mt-4" v-on:submit.prevent>
             <div class="form-group">
               <label for="jumlah_pemesanan">Jumlah Pesan</label>
-              <input type="number" class="form-control" v-model="pesan.jumlah_pemesanan"/>
+              <input
+                type="number"
+                class="form-control"
+                v-model="pesan.jumlah_pemesanan"
+              />
             </div>
             <div class="form-group">
-                <label for="keterangan">Keterangan</label>
-                <textarea v-model="pesan.keterangan" class="form-control" placeholder="Keterangan : Pedes , Nasi Setengah"></textarea>
+              <label for="keterangan">Keterangan</label>
+              <textarea
+                v-model="pesan.keterangan"
+                class="form-control"
+                placeholder="Keterangan : Pedes , Nasi Setengah"
+              ></textarea>
             </div>
-            <button type="submit" class="btn btn-success" @click="pemesanan"><b-icon-cart></b-icon-cart> Pesan </button>
+            <button type="submit" class="btn btn-success" @click="pemesanan">
+              <b-icon-cart></b-icon-cart> Pesan
+            </button>
           </form>
         </div>
       </div>
@@ -66,22 +76,25 @@ export default {
   data() {
     return {
       product: {},
-      pesan: {}
+      pesan: {},
     };
   },
   methods: {
     setProduct(data) {
       this.product = data;
     },
-    pemesanan () {
-this.pesan.products = this.product;
-axios
-.post("https://my-json-server.typicode.com/aditcahyono19/katalogmenu/keranjangs", this.pesan)
-.then (() => {
-    console.log("Berhasil");
-})
-.catch((err) => console.log(err))
-    }
+    pemesanan() {
+      this.pesan.products = this.product;
+      axios
+        .post(
+          "https://my-json-server.typicode.com/aditcahyono19/katalogmenu/keranjangs",
+          this.pesan
+        )
+        .then(() => {
+          console.log("Berhasil");
+        })
+        .catch((err) => console.log(err));
+    },
   },
   mounted() {
     axios
